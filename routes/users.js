1378533e71a2cb56d39a989e358dfd29dafcd75a
GustavoPerezYSSY <gustavo.perez@yssy.com.br>
2022-09-14 14:19:22 -0300

    sec-commit

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+function getRandomString(length) {
+  var result = '';
+  var characters = 'ABCDEFGHI JKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
+}
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  let results = [];
+  for (var ndx = 0; ndx < 1000; ndx++) {
+    results.push({ id: ndx + 1, name: getRandomString(30) })
+  }
+  res.send(results);
+});
+
+module.exports = router;
